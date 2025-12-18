721bb9cb837a5b607a3cd24138bfef7f856f331a
Adriano Regis <adbrum@outlook.com>
2025-12-18 16:21:10 +0000

    Refactor command execution for readability

--- tasks.py
+++ tasks.py
@@ -97,7 +97,8 @@ def qa(ctx):
         else:
             success('No lint to fix')
         info('Ensure PyPI can render README and CHANGELOG')
-        readme_results = ctx.run('python setup.py check -m -s', pty=True, warn=True, hide=True)
+        readme_results = ctx.run(
+            'python setup.py check -m -s', pty=True, warn=True, hide=True)
         if readme_results.failed:
             print(readme_results.stdout)
             error('README and/or CHANGELOG is not renderable by PyPI')
@@ -134,7 +135,8 @@ def update(ctx):
         ctx.run("pip install -r requirements/develop.pip")
         for requirement_file in ["udata", "install", "test", "develop"]:
             ctx.run(
-                f"pip-compile requirements/{requirement_file}.in --output-file=requirements/{requirement_file}.pip --upgrade"
+                f"pip-compile requirements/{requirement_file}.in "
+                f"--output-file=requirements/{requirement_file}.pip --upgrade"
             )
     # TODO: Add javascript dependencies update
 
