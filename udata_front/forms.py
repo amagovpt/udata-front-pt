1364b5eb3ccef2594c96c4403b2ec85f95184f35
gpsilv4 <goncalo_silv@hotmail.com>
2026-02-25 11:41:36 +0000

    feat: Implement unified user enumeration protection for login, password reset, and confirmation forms, alongside a new comprehensive test script.

--- udata_front/forms.py
+++ udata_front/forms.py
@@ -1,4 +1,4 @@
-from flask_security.forms import RegisterForm, SendConfirmationForm, ForgotPasswordForm
+from flask_security.forms import RegisterForm, SendConfirmationForm, ForgotPasswordForm, LoginForm
 from flask_wtf import recaptcha
 from udata.forms import fields
 from udata.forms import validators
@@ -18,8 +18,8 @@ class ExtendedRegisterForm(RegisterForm):
 class ExtendedSendConfirmationForm(SendConfirmationForm):
     recaptcha = recaptcha.RecaptchaField()
 
-    def validate(self):
-        if not super(ExtendedSendConfirmationForm, self).validate():
+    def validate(self, **kwargs):
+        if not super(ExtendedSendConfirmationForm, self).validate(**kwargs):
             # Se for um erro de email (ex: utilizador não existe), removemos o erro
             # para evitar enumeração de utilizadores.
             if 'email' in self.errors:
@@ -31,12 +31,25 @@ class ExtendedSendConfirmationForm(SendConfirmationForm):
 class ExtendedForgotPasswordForm(ForgotPasswordForm):
     recaptcha = recaptcha.RecaptchaField()
 
-    def validate(self):
-        if not super(ExtendedForgotPasswordForm, self).validate():
+    def validate(self, **kwargs):
+        if not super(ExtendedForgotPasswordForm, self).validate(**kwargs):
             # Se for um erro de email (ex: utilizador não existe), removemos o erro
             # para evitar enumeração de utilizadores.
             if 'email' in self.errors:
                 self.errors.pop('email')
                 return True
+            return False
+        return True
+class ExtendedLoginForm(LoginForm):
+    def validate(self, **kwargs):
+        if not super(ExtendedLoginForm, self).validate(**kwargs):
+            # Unificar mensagens de erro para evitar enumeração.
+            # Se houver erro no email ou senha, mostramos uma mensagem genérica.
+            if 'email' in self.errors or 'password' in self.errors:
+                self.errors.pop('email', None)
+                self.errors.pop('password', None)
+                # Adicionamos o erro genérico aos erros globais do formulário
+                self.form_errors.append(_('Invalid email or password.'))
+                return False
             return False
         return True
